d087fddd5a4eb7cbd522399bfd8f646d19d1d130
Lucija Oreški <loreski@tria.hr>
2019-11-06 10:52:12 +0100

    Added form.js

---
+++ src/screens/form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import Header from './components/JS/Header'
+import Pagination from './screens/Pagination'
+import Footer from './components/JS/Footer'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Pagination />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App
